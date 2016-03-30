407c66782532664610f33224393c5b68311da972
KevinBozic <bozic.kevin@gmail.com>
2016-03-30 10:43:24 -0400

    Display window

--- src/ants_clojure/core.clj
+++ src/ants_clojure/core.clj
@@ -1,5 +1,16 @@
 (ns ants-clojure.core
-  (:gen-class))
+  (:require [clojure.java.io :as io])
+  (:import [javafx.application Application]
+           [javafx.fxml FXMLLoader]
+           [javafx.scene Scene])
+  (:gen-class :extends javafx.application.Application))
+
+(defn -start [app stage]
+  (let [root (FXMLLoader/load (io/resource "main.fxml"))
+        scene (Scene. root 800 600)]
+    (.setTitle stage "Ants")
+    (.setScene stage scene)
+    (.show stage)))
 
 (defn -main []
-  (println "Hello, World!"))
+  (Application/launch ants_clojure.core (into-array String [])))
